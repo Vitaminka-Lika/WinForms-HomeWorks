306486d8669cf6cf6cfeef18062a7f2c88c278f7
Vitaminka-Lika <sarbassova_alika2001@mail.ru>
2017-09-26 14:38:08 +0600

    18/09/17
    Работа с датами

---
+++ DayOfTheWeek/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxWithDate = new System.Windows.Forms.TextBox();
+            this.textBoxWithDayOfTheWeek = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ButtonOk = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxWithDate
+            // 
+            this.textBoxWithDate.Location = new System.Drawing.Point(12, 37);
+            this.textBoxWithDate.Name = "textBoxWithDate";
+            this.textBoxWithDate.Size = new System.Drawing.Size(260, 20);
+            this.textBoxWithDate.TabIndex = 0;
+            this.textBoxWithDate.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBoxWithDayOfTheWeek
+            // 
+            this.textBoxWithDayOfTheWeek.Location = new System.Drawing.Point(12, 107);
+            this.textBoxWithDayOfTheWeek.Name = "textBoxWithDayOfTheWeek";
+            this.textBoxWithDayOfTheWeek.Size = new System.Drawing.Size(260, 20);
+            this.textBoxWithDayOfTheWeek.TabIndex = 1;
+            this.textBoxWithDayOfTheWeek.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Дата";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "День недели";
+            // 
+            // ButtonOk
+            // 
+            this.ButtonOk.Location = new System.Drawing.Point(197, 154);
+            this.ButtonOk.Name = "ButtonOk";
+            this.ButtonOk.Size = new System.Drawing.Size(75, 23);
+            this.ButtonOk.TabIndex = 4;
+            this.ButtonOk.Text = "Ok";
+            this.ButtonOk.UseVisualStyleBackColor = true;
+            this.ButtonOk.Click += new System.EventHandler(this.ButtonOk_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.BackColor = System.Drawing.Color.SeaShell;
+            this.ClientSize = new System.Drawing.Size(284, 193);
+            this.Controls.Add(this.ButtonOk);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxWithDayOfTheWeek);
+            this.Controls.Add(this.textBoxWithDate);
+            this.Name = "Form1";
+            this.Text = "Date";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxWithDate;
+        private System.Windows.Forms.TextBox textBoxWithDayOfTheWeek;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button ButtonOk;
+    }
+}
+
---
+++ DayOfTheWeek/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonOk_Click(object sender, EventArgs e)
+        {
+            DateTime current = new DateTime();
+            bool isOperationDone = DateTime.TryParse(textBoxWithDate.Text, out current);
+            if (isOperationDone)
+            {
+                textBoxWithDayOfTheWeek.Text = current.ToString("dddd");
+            }
+            else {
+                textBoxWithDayOfTheWeek.Text = "Ошибка ввода";
+            }
+        }
+    }
+}
