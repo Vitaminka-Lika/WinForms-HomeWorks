132b63f049cb9299b12a07f9755608ffb565d653
Vitaminka-Lika <sarbassova_alika2001@mail.ru>
2017-10-10 18:17:20 +0600

    21.09.2017
    Индикаторы: Загрузка данных человека в файл

---
+++ PersonalInformation/Dz/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Person person = new Person()
+            {
+                SecondName = textBox1.Text,
+                FirstName = textBox2.Text,
+                LastName=textBox3.Text
+            };
+
+            string json = JsonConvert.SerializeObject(person);
+            System.IO.File.WriteAllText(saveFileDialog1.FileName, json);
+        }
+    }
+}
