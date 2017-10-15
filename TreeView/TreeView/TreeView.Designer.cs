7b08ed4571ef392c7684c4c1bbd7673e2e4fbe58
Vitaminka-Lika <sarbassova_alika2001@mail.ru>
2017-10-15 17:17:29 +0600

    15/10/17
    Создайте приложение на базе TreeView для просмотра каталогов
    файлов с вложенными каталогами

---
+++ TreeView/TreeView/TreeView.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace TreeView
+{
+    partial class FileSystemForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.drivesSystemTreeView = new System.Windows.Forms.TreeView();
+            this.showButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // drivesSystemTreeView
+            // 
+            this.drivesSystemTreeView.Location = new System.Drawing.Point(12, 12);
+            this.drivesSystemTreeView.Name = "drivesSystemTreeView";
+            this.drivesSystemTreeView.Size = new System.Drawing.Size(260, 206);
+            this.drivesSystemTreeView.TabIndex = 0;
+            this.drivesSystemTreeView.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.DrivesSystemTreeView_BeforeExpand);
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(13, 226);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(259, 23);
+            this.showButton.TabIndex = 1;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = true;
+            // 
+            // FileSystemForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.drivesSystemTreeView);
+            this.Name = "FileSystemForm";
+            this.Text = "FileSystem";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView drivesSystemTreeView;
+        private System.Windows.Forms.Button showButton;
+    }
+}
+
---
+++ TreeView/TreeView/TreeView.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TreeView
+{
+    public partial class FileSystemForm : Form
+    {
+        public FileSystemForm()
+        {
+            InitializeComponent();
+            string[] drives = Environment.GetLogicalDrives();
+
+            foreach (string drive in drives)
+            {
+                DriveInfo driveInfo = new DriveInfo(drive);
+
+                TreeNode node = new TreeNode(drive.Substring(0, 1));
+                node.Tag = drive;
+
+                if (driveInfo.IsReady == true)
+                    node.Nodes.Add("...");
+
+                drivesSystemTreeView.Nodes.Add(node);
+            }
+        }
+
+        private void DrivesSystemTreeView_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            if (e.Node.Nodes.Count > 0)
+            {
+                if (e.Node.Nodes[0].Text == "..." && e.Node.Nodes[0].Tag == null)
+                {
+                    e.Node.Nodes.Clear();
+
+                    string[] directories = Directory.GetDirectories(e.Node.Tag.ToString());
+
+                    foreach (string directory in directories)
+                    {
+                        DirectoryInfo directoryInfo = new DirectoryInfo(directory);
+                        TreeNode node = new TreeNode(directoryInfo.Name, 0, 1);
+                       
+                        node.Tag = directory;
+                        
+                        if (directoryInfo.GetDirectories().Count() > 0)
+                            node.Nodes.Add(null, "...", 0, 0);
+
+                        foreach (var file in directoryInfo.GetFiles())
+                        {
+                            TreeNode n = new TreeNode(file.Name);
+                            node.Nodes.Add(n);
+                        }
+                        e.Node.Nodes.Add(node);
+                    }
+                }
+            }
+        }
+    }
+}
